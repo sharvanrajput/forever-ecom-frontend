aa57f6a96e1a568c5cf1526465cdcbee75362962
sharvanrajput <sharvanrajput098@gmail.com>
2025-11-22 15:33:38 +0530

    fix usercontext path

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ShopContextProvider } from './context/ShopContext'
+import { UserContextProvider } from "./context/userContext.jsx";
 import { ToastContainer } from 'react-toastify'
-import { UserContextProvider } from './context/userContext'
 
 
 createRoot(document.getElementById('root')).render(
